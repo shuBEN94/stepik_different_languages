668c7d68ab7511ae9b4b719804ee8ec1947feeae
shuBEN94 <shubinas@ya.ru>
2023-04-15 16:43:45 +0300

    test_added and changed conftest.py

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+
+def add_to_basket_button_check(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    add_to_basket_button = browser.find_elements(By.XPATH, '//*[@id="add_to_basket_form"]/button')
+    assert len(add_to_basket_button) > 0, 'button not found'
+
